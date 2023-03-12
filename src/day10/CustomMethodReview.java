d993e28927b8c0a919f3ad91dfa44f79cd3fa952
fyzpkl <feyizpekel@gmail.com>
2023-03-12 14:41:24 +0300

    Custom Methods return

--- src/day10/CustomMethodReview.java
+++ src/day10/CustomMethodReview.java
@@ -9,17 +9,29 @@ public class CustomMethodReview {
         int age = 10;
         //displayMessage(name, age);
         displayMessage1();
-        topla(10,25);
-        araTopla();
+        int x = araTopla();
+        System.out.println(araTopla());
+        kdvHesapla(araTopla());
+                        //900
+        vergiIade(kdvHesapla(5000));
+
     }
-    public static void araTopla(){
+    public static int araTopla(){
         int toplam=0;
         for (int i = 0; i < 11; i++) {
             toplam+=i;
         }
-        System.out.println("toplam = " + toplam);
+
+        return toplam;
+    }
+    public static void vergiIade(double para){
+
     }
 
+    public static double kdvHesapla(int para){
+        double hesap = para * .18;
+        return hesap;
+    }
 
     public static void topla(int num1, int num2){
         int num3 =20;
